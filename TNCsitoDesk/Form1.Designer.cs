b0fd5191ad9f954894a2c35fec5fc487424ded41
NRamosD <nixonramosdue@gmail.com>
2021-05-02 22:21:03 -0500

    subo proyecto

---
+++ TNCsitoDesk/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace TNCsitoDesk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notificacion = new System.Windows.Forms.NotifyIcon(this.components);
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notificacion
+            // 
+            this.notificacion.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notificacion.Text = "notifyIcon1";
+            this.notificacion.Visible = true;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(14, 34);
+            this.listBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(274, 139);
+            this.listBox.TabIndex = 0;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(294, 112);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(72, 29);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(294, 149);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(72, 24);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Notificaciones Recientes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 188);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.listBox);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TNCsito Dekstop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notificacion;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TNCsitoDesk/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+namespace TNCsitoDesk
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.DarkCyan;
+        }
+
+
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            
+            //"ubZ1tomtK1BFgdotVQoCJiDHE4zhaEvFdxoF4TiO"    "https://tncsito-135d4-default-rtdb.firebaseio.com/"
+            AuthSecret = ConfigurationManager.AppSettings["AUTH"],
+            BasePath = ConfigurationManager.AppSettings["BASE_PATH"]
+        };
+
+        IFirebaseClient client;
+        EventStreamResponse listener;
+        List<string> datos = new List<string>();
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null)
+            {
+                try
+                {
+                    eliminarTodoAlIniciar();
+                    await Task.Delay(2000);
+                }
+                catch(Exception exc)
+                {
+                    Console.WriteLine(exc);
+                }
+                
+                
+                setListener();
+                //MessageBox.Show("Conección establecida\n"+client.ToString());
+            }
+        }
+
+
+        //Métodos
+        async void setListener()
+        {
+            
+            listener = await client.OnAsync("req", (sender, args, context) => {
+                datos.Add(args.Data.ToString());
+                if (datos.Count == 3)
+                {
+                    //MessageBox.Show($"Entro {datos[0]} {datos[1]} {datos[2]}");
+                    notificar();
+                    
+                }              
+            });
+            
+        }
+        async void eliminarTodoAlIniciar()
+        {
+            FirebaseResponse response = await client.DeleteAsync("req");
+            //MessageBox.Show(""+response.StatusCode);
+        }
+        private async void LiveCall()
+        {
+            ///-MZJvf63pzVNCoAoVd_F
+            var res = await client.GetAsync("req/-MZJvf63pzVNCoAoVd_F");
+            //var objResponse1 = JsonConvert.DeserializeObject<List<Pedido>>(res.Body);
+            Pedido p = res.ResultAs<Pedido>();
+            //Pedido p = res.ResultAs<Pedido[]>();
+            MessageBox.Show("uhm\n" + p.tipo);
+        }
+        public void notificar()
+        {
+            int tipo= int.Parse(datos[2]);
+            string mensaje=datos[0];
+            string remitente = datos[1];
+            notificacion.Icon = new System.Drawing.Icon(Path.GetFullPath(@"../../imagen/ayay.ico"));
+            notificacion.Text = "Atiende tus notificaciones...";
+            notificacion.Visible = true;
+            switch (tipo)
+            {
+                case 1:
+                    notificacion.BalloonTipTitle = "Te estan llamando :/";//este importa
+                    notificacion.BalloonTipText = "Alguien necesita que vayas por algún motivo desconocido u.u";
+                    notificacion.ShowBalloonTip(3000);
+                    break;
+                case 2:
+                    notificacion.BalloonTipTitle = "Quieren que lleves algo...";//este importa
+                    notificacion.BalloonTipText = mensaje;//este importa
+                    notificacion.ShowBalloonTip(3000);
+                    break;
+                case 3:
+                    notificacion.BalloonTipTitle = "Alguien pregunta si...";//este importa
+                    notificacion.BalloonTipText = mensaje;//este importa
+                    notificacion.ShowBalloonTip(3000);
+                    break;
+                case 4:
+                    notificacion.BalloonTipTitle = "EMERGENCIA!!";//este importa
+                    notificacion.BalloonTipText = "Te están llamando, ve lo más rápido que puedas.";//este importa
+                    notificacion.ShowBalloonTip(3000);
+                    break;
+                default:
+                    break;
+
+            }
+            addListBox(mensaje, remitente,tipo);
+            datos = new List<string>();
+        }
+
+        private void addListBox(string men, string remi, int tipo)
+        {
+            string hora = DateTime.Now.ToString("hhmmss");
+            Thread TypingThread = new Thread(delegate () {
+
+                //heavyBackgroundTask();
+
+                // Cambiar el estado de los botones dentro del hilo TypingThread
+                // Esto no generará excepciones de nuevo !
+                if (listBox.InvokeRequired)
+                {
+                    listBox.Invoke(new MethodInvoker(delegate
+                    {
+                        switch (tipo)
+                        {
+                            case 1:
+                                listBox.Items.Add($"Quieren que vayas  ---  De: {remi} - {hora[0]}{hora[1]}:{hora[2]}{hora[3]}:{hora[4]}{hora[5]}");
+                                break;
+                            case 2:
+                                listBox.Items.Add($"Llevar {men}  ---  De: {remi} - {hora[0]}{hora[1]}:{hora[2]}{hora[3]}:{hora[4]}{hora[5]}");
+                                break;
+                            case 3:
+                                listBox.Items.Add($"¿{men}?  ---  De: {remi} - {hora[0]}{hora[1]}:{hora[2]}{hora[3]}:{hora[4]}{hora[5]}");
+                                break;
+                            case 4:
+                                listBox.Items.Add($"EMERGENCIA  ---  De: {remi} - {hora[0]}{hora[1]}:{hora[2]}{hora[3]}:{hora[4]}{hora[5]}");
+                                break;
+                            default:
+                                break;
+                        }
+                        
+                    }));
+                }
+            });
+            TypingThread.Start();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+    }
+    public class Pedido
+    {
+        public string mensaje { get; set; }
+        public string remitente { get; set; }
+        public int tipo { get; set; }
+    }
+}
